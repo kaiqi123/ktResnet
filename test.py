2195cdb76b6e459057c180a6aeee2e35fa573eee
zkqym <564049029@qq.com>
2019-02-21 14:57:42 -0700

    0219

--- test.py
+++ test.py
@@ -16,17 +16,23 @@ plt.show()
 batch_size = 128
 seed = 1234
 
+train_y = np.load("cifar10/pylearn2_gcn_whitened/train_labels.npy")
+print(train_y)
+print(train_y.shape)
+
 train_x = np.load("cifar10/pylearn2_gcn_whitened/train.npy")
 train_x = train_x.reshape((train_x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
 print(train_x.shape)
-image= train_x[7000]
-print(image.shape)
-train_image = tf.image.per_image_standardization(image)
+image_x = train_x[7000]
+image_y = train_y[7000]
+print(image_x.shape)
+print(image_y)
+image_x = tf.image.per_image_standardization(image_x)
 
 min_after_dequeue = 10000
 capacity = min_after_dequeue + 3 * batch_size
-example_batch= tf.train.shuffle_batch (
-        [train_image], batch_size=batch_size, capacity=capacity,
+feature_batch, label_batch = tf.train.shuffle_batch (
+        [image_x, image_y], batch_size=batch_size, capacity=capacity,
             min_after_dequeue=min_after_dequeue, seed=seed)
 
 with tf.Session() as sess:
@@ -36,9 +42,9 @@ with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
 
     for i in range(1):
-        images_feed = sess.run(example_batch)
+        images_feed, labels_feed = sess.run([feature_batch, label_batch])
         print(images_feed.shape)
-        #print(labels_feed)
+        print(labels_feed)
 
     coord.request_stop()
     coord.join(threads)
