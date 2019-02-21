a066eec30313fdef8a746bca705a705338dcce6e
zkqym <564049029@qq.com>
2019-02-20 21:16:41 -0700

    0219

--- convert_pkl_to_image.py
+++ convert_pkl_to_image.py
@@ -7,12 +7,17 @@ def unpickle(file):
         dict = cPickle.load(fo)
     return dict
 
-file = "cifar10/pylearn2_gcn_whitened/test.pkl"
-dict = unpickle(file)
-print(dict)
 
 train = CIFAR10(which_set='train', gcn=55.)
 print(train.X.shape)
-print(type(train.X))
+print(train.X)
 print(train.y.shape)
-print(type(train.y))
+print(train.y)
+
+file = "cifar10/pylearn2_gcn_whitened/train.pkl"
+o = unpickle(file)
+print(o)
+print(o.X.shape)
+print(o.X)
+print(o.y.shape)
+print(o.y)
