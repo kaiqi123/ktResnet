db1a3a923c257253d413bab8b523faf493579d50
zkqym <564049029@qq.com>
2019-02-21 16:53:32 -0700

    0219

--- test2.py
+++ test2.py
@@ -14,5 +14,21 @@ x = np.load("cifar10/pylearn2_gcn_whitened/train.npy")
 x = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
 print(x.shape)
 
+image_list = []
+for i in range(x.shape[0]):
+    one = {}
+    one["feature"] = x[i]
+    one["label"] = y[i]
+    image_list.append(one)
+print(len(image_list))
+
+image_queue = tf.train.input_producer(image_list)
+min_after_dequeue = 10000
+capacity = min_after_dequeue + 3 * batch_size
+image_batch = tf.train.shuffle_batch(
+    image_queue, batch_size=batch_size, capacity=capacity,
+            min_after_dequeue=min_after_dequeue, seed=seed)
+
+print(image_batch)
 
 print("1111111")
