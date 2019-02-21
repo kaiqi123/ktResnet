926d98da1c88ca08cf2bbbf5d300ae10f3536903
zkqym <564049029@qq.com>
2019-02-21 14:05:36 -0700

    0219

--- test.py
+++ test.py
@@ -15,16 +15,6 @@ plt.show()
 train_x = np.load("cifar10/pylearn2_gcn_whitened/train.npy")
 train_x = train_x.reshape((train_x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
 print(train_x.shape)
-image= train_x[7000]
+image= train_x
 print(image.shape)
 train_image = tf.image.per_image_standardization(image)
-print(train_image)
-
-"""
-train_image = tf.image.per_image_standardization(train_image)
-train_image = tf.image.resize_image_with_pad(train_image, self.image_width + self.pad, self.image_width + self.pad)
-train_image = tf.image.random_flip_left_right(train_image)
-print(train_image)
-self.train_image = tf.random_crop(train_image, [self.image_height, self.image_width, 3], seed=self.seed, name="crop")
-print(self.train_image)
-"""
