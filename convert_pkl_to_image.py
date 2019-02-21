eee3752c6f84e7e3909edef19e0f9654702787ab
zkqym <564049029@qq.com>
2019-02-20 22:11:29 -0700

    0219

--- convert_pkl_to_image.py
+++ convert_pkl_to_image.py
@@ -1,5 +1,5 @@
 import cPickle
-# from pylearn2.datasets.cifar10 import CIFAR10
+from pylearn2.datasets.cifar10 import CIFAR10
 import numpy as np
 
 def unpickle(file):
@@ -9,12 +9,12 @@ def unpickle(file):
     return dict
 
 
-#train = CIFAR10(which_set='train', gcn=55.)
-#print(train.X.shape)
-#print(train.X)
-#print(train.y.shape)
-#print(train.y)
-
+train = CIFAR10(which_set='train', gcn=55.)
+x = train.X
+y_o = train.y
+features_origin = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
+np.save("temp/temp_origin", features_origin[0])
+print(features_origin.shape)
 
 file = "cifar10/pylearn2_gcn_whitened/train.pkl"
 o = unpickle(file)
@@ -22,5 +22,5 @@ x = o.X
 y = o.y
 
 features = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
-np.save("temp/temp", features[0])
+np.save("temp/temp_whiten", features[0])
 print(features.shape)
---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.load("temp/temp.npy")
+print(a.shape)
+plt.imshow(a)
+plt.show()
