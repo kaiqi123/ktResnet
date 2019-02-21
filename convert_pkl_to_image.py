a650a38ece93a21f77426de7b601b6bbd1107ecb
zkqym <564049029@qq.com>
2019-02-20 21:30:45 -0700

    0219

--- convert_pkl_to_image.py
+++ convert_pkl_to_image.py
@@ -1,5 +1,6 @@
 import cPickle
-from pylearn2.datasets.cifar10 import CIFAR10
+# from pylearn2.datasets.cifar10 import CIFAR10
+
 
 def unpickle(file):
 
@@ -7,18 +8,22 @@ def unpickle(file):
         dict = cPickle.load(fo)
     return dict
 
-print(CIFAR10.convert2image(CIFAR10.traintensor(1)))
 
-train = CIFAR10(which_set='train', gcn=55.)
-print(train.X.shape)
-print(train.X)
-print(train.y.shape)
-print(train.y)
+#train = CIFAR10(which_set='train', gcn=55.)
+#print(train.X.shape)
+#print(train.X)
+#print(train.y.shape)
+#print(train.y)
 
 file = "cifar10/pylearn2_gcn_whitened/train.pkl"
 o = unpickle(file)
-print(o)
-print(o.X.shape)
-print(o.X)
-print(o.y.shape)
-print(o.y)
+x = o.X
+y = o.y
+#print(o)
+#print(o.X.shape)
+#print(o.X)
+#print(o.y.shape)
+#print(o.y)
+features = x.reshape((len(x.shape[0]), 3, 32, 32)).transpose(0, 2, 3, 1)
+print(x.shape[0])
+print(features)
