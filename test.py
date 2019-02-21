92dd24612ddb151235ec00630f6a12eca398949e
zkqym <564049029@qq.com>
2019-02-21 15:49:02 -0700

    0219

--- test.py
+++ test.py
@@ -16,23 +16,35 @@ plt.show()
 batch_size = 128
 seed = 1234
 
-train_y = np.load("cifar10/pylearn2_gcn_whitened/train_labels.npy")
-print(train_y)
-print(train_y.shape)
+y = np.load("cifar10/pylearn2_gcn_whitened/train_labels.npy")
+print(y.shape)
 
-train_x = np.load("cifar10/pylearn2_gcn_whitened/train.npy")
-train_x = train_x.reshape((train_x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
-print(train_x.shape)
-image_x = train_x[7000]
-image_y = train_y[7000]
+x = np.load("cifar10/pylearn2_gcn_whitened/train.npy")
+x = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
+print(x.shape)
+
+"""
+image_x = x[7000]
+image_y = y[7000]
 print(image_x.shape)
 print(image_y)
 image_x = tf.image.per_image_standardization(image_x)
+tf.train.input_producer()
+"""
 
+#feature_list = []
+image_list = []
+for i in range(x.shape[0]):
+    one = {}
+    one["feature"] = x[i]
+    one["label"] = y[i]
+    image_list.append(one)
+
+image_queue = tf.train.input_producer(image_list)
 min_after_dequeue = 10000
 capacity = min_after_dequeue + 3 * batch_size
-feature_batch, label_batch = tf.train.shuffle_batch (
-        [image_x, image_y], batch_size=batch_size, capacity=capacity,
+image_batch = tf.train.shuffle_batch(
+    image_queue, batch_size=batch_size, capacity=capacity,
             min_after_dequeue=min_after_dequeue, seed=seed)
 
 with tf.Session() as sess:
@@ -42,9 +54,9 @@ with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
 
     for i in range(1):
-        images_feed, labels_feed = sess.run([feature_batch, label_batch])
-        print(images_feed.shape)
-        print(labels_feed)
+        image_feed = sess.run(image_batch)
+        print(image_feed)
+        # print(labels_feed)
 
     coord.request_stop()
     coord.join(threads)
