cdb57fd291808ee58a539429cf902de9cfdaa626
zkqym <564049029@qq.com>
2019-02-21 14:45:03 -0700

    0219

--- convert_pkl_to_image.py
+++ convert_pkl_to_image.py
@@ -10,12 +10,18 @@ def unpickle(file):
 
 
 train = CIFAR10(which_set='train', gcn=55.)
-x = train.X
-y_o = train.y
-features_origin = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
+features_origin = train.X.reshape((train.X.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
 np.save("temp/temp_origin", features_origin[7000])
 print(features_origin.shape)
 
+np.save("cifar10/pylearn2_gcn_whitened/train_labels", train.y)
+print(train.y.shape)
+
+test = CIFAR10(which_set='test', gcn=55.)
+np.save("cifar10/pylearn2_gcn_whitened/test_labels", test.y)
+print(test.y.shape)
+
+"""
 file = "cifar10/pylearn2_gcn_whitened/train.pkl"
 o = unpickle(file)
 x = o.X
@@ -24,3 +30,4 @@ y = o.y
 features = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
 np.save("temp/temp_whiten", features[7000])
 print(features.shape)
+"""
