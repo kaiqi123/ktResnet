593722663cdc0427597663c287ac5008341e6aeb
zkqym <564049029@qq.com>
2019-02-22 14:30:27 -0700

    0219

--- test2.py
+++ test2.py
@@ -6,15 +6,19 @@ batch_size = 128
 seed = 1234
 
 y = np.load("cifar10/pylearn2_gcn_whitened/train_labels.npy")
-print(y.shape)
+print(y)
+y = list(y)
+print(y)
 
+"""
 x = np.load("cifar10/pylearn2_gcn_whitened/train.npy")
 x = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
 print(x.shape)
 
 output_dir = "cifar10_npy/train"
 serial.mkdir(output_dir)
-for i in range(x.shape[0]):
-    np.save(output_dir+"/"+str(y[i])+",train"+str(i), x[i])
-
+#for i in range(x.shape[0]):
+i = 0
+np.save(output_dir+"/"+str(y[i])+",train"+str(i), x[i])
+"""
 
