819dbca4ca739d873b568eac5b178f23eb6e7481
zkqym <564049029@qq.com>
2019-02-22 14:33:11 -0700

    0219

--- test2.py
+++ test2.py
@@ -6,11 +6,9 @@ batch_size = 128
 seed = 1234
 
 y = np.load("cifar10/pylearn2_gcn_whitened/train_labels.npy")
-print(y[0])
-#y = list(y)
-print(y[0][0])
+print(y.shape)
 
-"""
+
 x = np.load("cifar10/pylearn2_gcn_whitened/train.npy")
 x = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
 print(x.shape)
@@ -19,6 +17,6 @@ output_dir = "cifar10_npy/train"
 serial.mkdir(output_dir)
 #for i in range(x.shape[0]):
 i = 0
-np.save(output_dir+"/"+str(y[i])+",train"+str(i), x[i])
-"""
+np.save(output_dir+"/"+str(y[i][0])+",train"+str(i), x[i])
+
 
