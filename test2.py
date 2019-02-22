9f510ff0de0fcd96d10a15a07e39f68e4c480ea2
zkqym <564049029@qq.com>
2019-02-22 14:43:18 -0700

    0219

--- test2.py
+++ test2.py
@@ -2,12 +2,8 @@ import numpy as np
 import tensorflow as tf
 from pylearn2.utils import serial
 
-batch_size = 128
-seed = 1234
-
 y = np.load("cifar10/pylearn2_gcn_whitened/train_labels.npy")
 print(y.shape)
-
 
 x = np.load("cifar10/pylearn2_gcn_whitened/train.npy")
 x = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
@@ -15,8 +11,14 @@ print(x.shape)
 
 output_dir = "cifar10_npy/train"
 serial.mkdir(output_dir)
-#for i in range(x.shape[0]):
-i = 0
-np.save(output_dir+"/"+str(y[i][0])+",train"+str(i), x[i])
+file_names = []
+for i in range(x.shape[0]):
+    name = output_dir + "/" + str(y[i][0]) + ",train" + str(i)
+    np.save(name, x[i])
+    file_names.append(name)
+
+open("cifar10_npy/cifar10-train.txt", "w").writelines(file_names)
+print(len(file_names))
+
 
 
