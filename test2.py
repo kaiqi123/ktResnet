cae65a183e4539b7dbd671eceb295760e0bfe24f
zkqym <564049029@qq.com>
2019-02-21 18:48:24 -0700

    0219

--- test2.py
+++ test2.py
@@ -12,11 +12,11 @@ x = np.load("cifar10/pylearn2_gcn_whitened/train.npy")
 x = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
 print(x.shape)
 
-images_list = []
-for i in range(x.shape[0]):
-    one = tf.convert_to_tensor([x[i], y[i]])
-    images_list.append(one)
+#images_list = []
+#for i in range(x.shape[0]):
+#    one = tf.convert_to_tensor([x[i], y[i]])
+#    images_list.append(one)
 
-image = tf.train.slice_input_producer(images_list)
+image = tf.train.slice_input_producer([x[0], y[0]])
 print(image)
 
