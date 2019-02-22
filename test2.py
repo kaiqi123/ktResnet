06d037097fc1c64358aa237a8af870cfa49e1c39
zkqym <564049029@qq.com>
2019-02-22 14:19:34 -0700

    0219

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-# import matplotlib.pyplot as plt
+from pylearn2.utils import serial
 
 batch_size = 128
 seed = 1234
@@ -12,11 +12,6 @@ x = np.load("cifar10/pylearn2_gcn_whitened/train.npy")
 x = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
 print(x.shape)
 
-#images_list = []
-#for i in range(x.shape[0]):
-#    one = tf.convert_to_tensor([x[i], y[i]])
-#    images_list.append(one)
-
-image = tf.train.slice_input_producer([x[0], y[0]])
-print(image)
+output_dir = "ciafr10_npy/train"
+serial.mkdir(output_dir)
 
