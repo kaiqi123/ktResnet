9d5b84f7f1d744aca19ed86d377baacf91934c3d
zkqym <564049029@qq.com>
2019-02-22 16:44:53 -0700

    0219

--- test.py
+++ test.py
@@ -17,17 +17,15 @@ print(os.getcwd())
 filename_queue = tf.train.string_input_producer([test_dataset], num_epochs=None)
 reader = tf.TextLineReader()
 key_temp, value_temp = reader.read(filename_queue)
-print(value_temp)
 record_defaults = [[1], ['']]
 col1, col2 = tf.decode_csv(value_temp, record_defaults=record_defaults)
-print(col1, col2)
 
-file_content = tf.read_file(col2)
+# file_content = tf.read_file(col2)
 # file_content = np.load(col2)
-print(file_content)
+# print(file_content)
 
-train_image = tf.image.per_image_standardization(file_content)
 """
+train_image = tf.image.per_image_standardization(file_content)
 train_image = tf.image.resize_image_with_pad(train_image, image_width + pad, image_width + pad)
 train_image = tf.image.random_flip_left_right(train_image)
 train_image = tf.random_crop(train_image, [image_height, image_width, 3], seed=seed, name="crop")
@@ -56,8 +54,8 @@ with tf.Session() as sess:
         print(type(labels_feed))
         """
 
-        images_feed = sess.run(train_image)
-        print(images_feed)
+        col1, col2 = sess.run([col1, col2])
+        print(col1, col2)
 
     coord.request_stop()
     coord.join(threads)
