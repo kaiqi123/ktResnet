f53e099d78f11eb85ee852a231ba0d9139c3245e
zkqym <564049029@qq.com>
2019-02-22 16:27:17 -0700

    0219

--- data_prepare/deal_whiten_npy_file.py
+++ data_prepare/deal_whiten_npy_file.py
@@ -16,7 +16,7 @@ def deal_npy_file(whitenFile_label, whitenFile_feature, txtfile, mode):
     serial.mkdir(output_dir)
     file_names = []
     for i in range(x.shape[0]):
-        np.save(output_dir + "/" + str(y[i][0]) + "," + mode + str(i), x[i])
+        np.save(output_dir + "/" + mode + str(i), x[i])
         file_names.append(str(y[i][0]) + "," + output_dir + "/" + mode + str(i) + ".npy" + "\n")
 
     open(txtfile, "w").writelines(file_names)
@@ -39,6 +39,6 @@ deal_npy_file(whitenFile_label, whitenFile_feature, txtfile, mode)
 whitenFile_label = "./cifar10/pylearn2_gcn_whitened/test_labels.npy"
 whitenFile_feature = "./cifar10/pylearn2_gcn_whitened/test.npy"
 # ouput file
-txtfile = "./cifar10_npy/cifar10-test.txt"
+txtfile = "./cifar10_input/cifar10-test.txt"
 mode = "test"
 deal_npy_file(whitenFile_label, whitenFile_feature, txtfile, mode)
