7f06ab133ff47a01148ed794a5887e39d2058b08
zkqym <564049029@qq.com>
2019-02-22 15:46:33 -0700

    0219

--- data_prepare/deal_whiten_npy_file.py
+++ data_prepare/deal_whiten_npy_file.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pylearn2.utils import serial
-
+import os
 
 # convert_whiten npyFile to a txtfile for input and multi Npy Files
 def deal_npy_file(whitenFile_label, whitenFile_feature, txtfile, mode):
@@ -11,7 +11,7 @@ def deal_npy_file(whitenFile_label, whitenFile_feature, txtfile, mode):
     x = x.reshape((x.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
     print(x.shape)
 
-    output_dir = "./cifar10_npy/" + mode
+    output_dir = "cifar10_npy/" + mode
     serial.mkdir(output_dir)
     file_names = []
     for i in range(x.shape[0]):
@@ -22,18 +22,20 @@ def deal_npy_file(whitenFile_label, whitenFile_feature, txtfile, mode):
     print(len(file_names))
 
 
+os.chdir("~/ktResnet/")
+
 # input file
-whitenFile_label = "./cifar10/pylearn2_gcn_whitened/train_labels.npy"
-whitenFile_feature = "./cifar10/pylearn2_gcn_whitened/train.npy"
+whitenFile_label = "cifar10/pylearn2_gcn_whitened/train_labels.npy"
+whitenFile_feature = "cifar10/pylearn2_gcn_whitened/train.npy"
 # ouput file
-txtfile = "./cifar10_npy/cifar10-train.txt"
+txtfile = "cifar10_npy/cifar10-train.txt"
 mode = "train"
 deal_npy_file(whitenFile_label, whitenFile_feature, txtfile, mode)
 
 
-whitenFile_label = "./cifar10/pylearn2_gcn_whitened/test_labels.npy"
-whitenFile_feature = "./cifar10/pylearn2_gcn_whitened/test.npy"
+whitenFile_label = "cifar10/pylearn2_gcn_whitened/test_labels.npy"
+whitenFile_feature = "cifar10/pylearn2_gcn_whitened/test.npy"
 # ouput file
-txtfile = "./cifar10_npy/cifar10-test.txt"
+txtfile = "cifar10_npy/cifar10-test.txt"
 mode = "test"
 deal_npy_file(whitenFile_label, whitenFile_feature, txtfile, mode)
