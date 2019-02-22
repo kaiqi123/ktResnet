60a0a05e961ed9e6c0b9748a506ca0dab68d4847
zkqym <564049029@qq.com>
2019-02-22 16:37:44 -0700

    0219

--- test.py
+++ test.py
@@ -22,12 +22,12 @@ record_defaults = [[1], ['']]
 col1, col2 = tf.decode_csv(value_temp, record_defaults=record_defaults)
 print(col1, col2)
 
-file_content = tf.read_file(col2)
+# file_content = tf.read_file(col2)
+file_content = np.load(col2)
 print(file_content)
 
+train_image = tf.image.per_image_standardization(file_content)
 """
-train_image = tf.image.decode_png(file_content, channels=num_channels)
-train_image = tf.image.per_image_standardization(train_image)
 train_image = tf.image.resize_image_with_pad(train_image, image_width + pad, image_width + pad)
 train_image = tf.image.random_flip_left_right(train_image)
 train_image = tf.random_crop(train_image, [image_height, image_width, 3], seed=seed, name="crop")
@@ -38,7 +38,7 @@ capacity = min_after_dequeue + 3 * batch_size
 example_batch, label_batch = tf.train.shuffle_batch (
         [train_image, col1], batch_size=batch_size, capacity=capacity,
             min_after_dequeue=min_after_dequeue, seed=seed)
-
+"""
 
 with tf.Session() as sess:
 
@@ -47,14 +47,17 @@ with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
 
     for i in range(1):
-        #images_feed, labels_feed = sess.run([data_input_test.example_batch, data_input_test.label_batch])
 
+        """
         images_feed, labels_feed = sess.run([example_batch, label_batch])
         print(images_feed.shape)
         print(labels_feed)
         print(type(images_feed))
         print(type(labels_feed))
+        """
+
+        images_feed = sess.run(train_image)
+        print(images_feed)
 
     coord.request_stop()
     coord.join(threads)
-"""
