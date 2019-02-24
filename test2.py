035df331590bc42d7831ba0691773b0c1fa92a4e
zkqym <564049029@qq.com>
2019-02-23 22:00:10 -0700

    0219

--- test2.py
+++ test2.py
@@ -23,6 +23,7 @@ def deal_npy_file(whitenFile_label, whitenFile_feature, txtfile, mode):
     #for i in range(x.shape[0]):
 
     img = x[i]
+    img = Image.fromarray(img.astype(np.uint8))
     #i0 = Image.fromarray(img[0]).convert('L')
     #i1 = Image.fromarray(img[1]).convert('L')
     #i2 = Image.fromarray(img[2]).convert('L')
