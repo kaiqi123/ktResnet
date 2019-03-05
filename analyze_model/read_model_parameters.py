0ca88a735dca35616fc04954affff9fd5784fffd
kzhao27 <kzhao27@asu.edu>
2019-03-05 13:02:37 -0700

    0305

--- analyze_model/read_model_parameters.py
+++ analyze_model/read_model_parameters.py
@@ -1,24 +1,30 @@
 import re
 import torch
-import torch.nn.functional as F
-from torch.autograd import Variable
-#from torchviz import make_dot
-#from torch.utils import model_zoo
+# import torch.nn.functional as F
+# from torch.autograd import Variable
+# from torchviz import make_dot
+# from torch.utils import model_zoo
 
-
-# params = model_zoo.load_url('model.pt7')
+def print_tensor_dict(params):
+    kmax = max(len(key) for key in params.keys())
+    #for i, (key, v) in enumerate(params.items()):
+    for i, (key, v) in enumerate(sorted(params.items())):
+        print(str(i).ljust(5), key.ljust(kmax + 3), str(tuple(v.shape)).ljust(23), torch.typename(v), v.requires_grad)
 
 params = torch.load('model.pt7')
 
 print(type(params))
 print(type(params.items()))
-print(params.items()[0])
+print_tensor_dict(params)
+
+"""
+# params = model_zoo.load_url('https://s3.amazonaws.com/modelzoo-networks/wide-resnet-50-2-export-5ae25d50.pth')
 
 # convert numpy arrays to torch Variables
 for k, v in sorted(params.items()):
     print(k, tuple(v.shape))
     params[k] = Variable(v, requires_grad=True)
 
-# print('\nTotal parameters:', sum(v.numel() for v in params.values()))
-
+print('\nTotal parameters:', sum(v.numel() for v in params.values()))
+"""
 
