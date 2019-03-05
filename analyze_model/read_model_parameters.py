e3b2cdbb2563f193e0d9b63db62d7e968472bb0e
kzhao27 <kzhao27@asu.edu>
2019-03-05 16:51:33 -0700

    0305

--- analyze_model/read_model_parameters.py
+++ analyze_model/read_model_parameters.py
@@ -16,10 +16,11 @@ checkpoint = torch.load('model.pt7')
 
 print(checkpoint.keys())
 
-params = checkpoint['params']
+params_pytorch = checkpoint['params']
 
-np.save(params, 'model_w10d28')
-params = np.load('model_w10d28')
+np.save('model_w10d28.npy', params_pytorch)
+
+params = np.load('model_w10d28.npy')
 
 for k, v in sorted(params.items()):
     print(k, tuple(v.shape))
