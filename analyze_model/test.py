8b52fc9792c846613ffcd8770206bfb6e094fb72
kzhao27 <kzhao27@asu.edu>
2019-03-07 22:44:24 -0700

    0305

--- analyze_model/test.py
+++ analyze_model/test.py
@@ -16,6 +16,8 @@ def g(inputs, params):
         return v
 
     params = {k: tf.constant(tr(v)) for k, v in params.items()}
+    for k, v in sorted(params.items()):
+        print(k, tuple(v.shape))
 
     def conv2d(x, params, name, stride=1, padding=0):
         x = tf.pad(x, [[0, 0], [padding, padding], [padding, padding], [0, 0]])
