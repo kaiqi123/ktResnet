e86d3fb6f2e37c241ac21fb522fe1c5e73dfb16e
kzhao27 <kzhao27@asu.edu>
2019-03-08 14:58:49 -0700

    0305

--- test.py
+++ test.py
@@ -3,8 +3,13 @@ import tensorflow as tf
 init = tf.constant_initializer(value=0)
 print(init)
 
+"""
 print('fitting shape:')
 with tf.Session():
     x = tf.get_variable('x', initializer=init)
     x.initializer.run()
     print(x.eval())
+"""
+sess = tf.Session()
+a = sess.run(init)
+print(a)
