60b2279f4821d1b6cdcafcdc2ae0fe4a16e4af11
kzhao27 <kzhao27@asu.edu>
2019-03-11 20:36:43 -0700

    0305

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ class Resnet(object):
             print(device_lib.list_local_devices())
 
             # This line allows the code to use only sufficient memory and does not block entire GPU
-            #config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
+            config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
 
             # set the seed so that we have same loss values and initializations for every run.
             tf.set_random_seed(SEED)
