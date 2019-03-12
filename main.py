f518bcdb387757b73ffacea363ef7aba15e6032b
kzhao27 <kzhao27@asu.edu>
2019-03-11 20:37:43 -0700

    0305

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
 from DataInput import DataInput
-# from ModelConstruct import Model
-from ModelConstruct_Pretrained import Model
+from ModelConstruct import Model
+# from ModelConstruct_Pretrained import Model
 import time
 import sys
 import argparse
@@ -89,8 +89,8 @@ class Resnet(object):
 
         print("Define Teacher")
         mentor = Model(FLAGS.num_channels, SEED)
-        # mentor_data_dict = mentor.build_teacher_model(images_placeholder, FLAGS.num_classes, Widen_Factor, TeacherModel_N, phase_train)
-        mentor_data_dict = mentor.build_teacher_model(images_placeholder, TeacherModel_N, phase_train)
+        mentor_data_dict = mentor.build_teacher_model(images_placeholder, FLAGS.num_classes, Widen_Factor, TeacherModel_N, phase_train)
+        # mentor_data_dict = mentor.build_teacher_model(images_placeholder, TeacherModel_N, phase_train)
         self.loss = mentor.loss(labels_placeholder)
 
         #steps_per_epoch = FLAGS.num_examples_per_epoch_for_train / FLAGS.batch_size
